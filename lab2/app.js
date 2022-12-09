55c7c374a7219f6fa1046344ea1947ab207a92c7
AndiiSkosyr <andrewskosyr@gmail.com>
2022-12-09 14:48:12 +0200

    commit 2

--- lab2/app.js
+++ lab2/app.js
@@ -1,17 +1,8 @@
 const express = require("express");
- 
 const app = express();
-app.use(function(request, response, next){
-    console.log("Middleware 1");
-    next();
-});
-app.use(function(request, response, next){
-    console.log("Middleware 2");
-    next();
+ 
+app.use(function (request, response) {
+  response.sendFile(__dirname + "/index.html");
 });
  
-app.get("/", function(request, response){
-    console.log("Route /");
-    response.send("Hello");
-});
 app.listen(3000);
